56fb5eee25f61ec63fcfc050fc038733f1b7bfbc
Marcin Drozd <marcindrozd@me.com>
2017-05-29 11:42:35 +0200

    Add readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Robot
+
+## Version 1
+
+1. Run `ruby robot.rb` from the irb console to execute some commands
+1. Or go to `irb`, then `require_relative robot` and provide your own commands in the following form:
+* place 1, 1, EAST
+* move
+* report
+
+To execute test scripts, run `ruby robot_test.rb`
--- robot.rb
+++ robot.rb
@@ -53,3 +53,40 @@ end
 def robot_outside_the_grid?(x, y)
   x < MIN_X || x > MAX_X || y < MIN_Y || y > MAX_Y
 end
+
+
+# ---------------------
+# TEST DATA
+# ---------------------
+
+if __FILE__ == $0
+  puts "Placing robot..."
+  place 1, 2, EAST
+
+  puts "Moving to the east..."
+  move
+
+  puts "Moving to the east..."
+  move
+
+  puts "Turning left..."
+  left
+
+  puts "Moving to the north..."
+  move
+
+  puts "Moving to the north..."
+  move
+
+  puts "Turning right..."
+  right
+
+  puts "Turning right..."
+  right
+
+  puts "Moving to the south..."
+  move
+
+  puts "Reporting position..."
+  puts report
+end
